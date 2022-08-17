3b2de168566549ba49d0a53ded0e2526f177a272
fit2-zhao <yong.zhao@fit2cloud.com>
2022-08-17 16:53:12 +0800

    refactor(文件管理): jar加载优化

--- src/main/java/io/metersphere/jmeter/MsClassLoader.java
+++ src/main/java/io/metersphere/jmeter/MsClassLoader.java
@@ -1,9 +1,9 @@
 package io.metersphere.jmeter;
 
 import com.alibaba.fastjson.JSON;
-import groovy.lang.GroovyClassLoader;
 import io.metersphere.utils.LoggerUtil;
 import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang.StringUtils;
 
 import java.io.File;
 import java.net.URL;
@@ -18,32 +18,19 @@ public class MsClassLoader {
         MsDynamicClassLoader urlClassLoader = new MsDynamicClassLoader();
         jarPaths.forEach(path -> {
             try {
-                File jarFile = new File(path);
-                URL jarUrl = jarFile.toURI().toURL();
-                urlClassLoader.addURLFile(jarUrl);
+                if (StringUtils.isNotEmpty(path)) {
+                    File jarFile = new File(path);
+                    if (jarFile.exists()) {
+                        URL jarUrl = jarFile.toURI().toURL();
+                        urlClassLoader.addURLFile(jarUrl);
+                    }
+                }
                 LoggerUtil.info("完成初始化JAR[ " + path + " ]");
             } catch (Exception ex) {
                 LoggerUtil.error("加载JAR包失败：", ex);
             }
         });
         return urlClassLoader;
-    }
-
-    public static void loadJar(List<String> jarPaths, GroovyClassLoader classLoader) {
-        if (CollectionUtils.isEmpty(jarPaths)) {
-            return;
-        }
-        LoggerUtil.info("开始初始化GroovyJAR[ " + JSON.toJSONString(jarPaths) + " ]");
-        jarPaths.forEach(path -> {
-            try {
-                File jarFile = new File(path);
-                URL jarUrl = jarFile.toURI().toURL();
-                classLoader.addURL(jarUrl);
-                LoggerUtil.info("完成初始化JAR[ " + path + " ]");
-            } catch (Exception ex) {
-                LoggerUtil.error("加载JAR包失败：", ex);
-            }
-        });
     }
 
     private MsClassLoader() {
