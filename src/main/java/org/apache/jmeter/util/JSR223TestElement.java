eefd3097d76bf0583447e8ba1c3c589bfce63a81
周文庆 <zhwq1216@163.com>
2022-12-14 12:55:34 +0800

    修正groovy脚本编译没有缓存导致tps下降问题, https://github.com/metersphere/metersphere/issues/20630

--- src/main/java/org/apache/jmeter/util/JSR223TestElement.java
+++ src/main/java/org/apache/jmeter/util/JSR223TestElement.java
@@ -193,7 +193,8 @@ public abstract class JSR223TestElement extends ScriptingTestElement
         File scriptFile = new File(getFilename());
         // Hack: bsh-2.0b5.jar BshScriptEngine implements Compilable but throws
         // "java.lang.Error: unimplemented"
-        boolean supportsCompilable = false;
+        boolean supportsCompilable = scriptEngine instanceof Compilable
+            && !"bsh.engine.BshScriptEngine".equals(scriptEngine.getClass().getName()); // NOSONAR // $NON-NLS-1$
 
         ClassLoader contextClassLoader = Thread.currentThread().getContextClassLoader();
         // 将当前类加载器设置为 loader ，解决由系统类加载器加载的 JMeter 无法动态加载 jar 包问题
